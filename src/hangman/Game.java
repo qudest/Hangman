fd8de8c557225becbac964170a1ff270d9fd88b1
nagornyy_a_s <nagornyi.arsenii@mail.ru>
2024-02-01 14:02:24 +0300

    Add words reader

--- src/hangman/Game.java
+++ src/hangman/Game.java
@@ -1,6 +1,7 @@
 package hangman;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Game {
     private static final String[] MENU = {"1. Начать новую игру",
@@ -13,7 +14,22 @@ public class Game {
         System.out.println();
     }
 
+    private static List<String> readWords(File file) {
+        List<String> words = new ArrayList<>();
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String[] s = scanner.nextLine().split(" ");
+                words.addAll(Arrays.asList(s));
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.exit(0);
+        }
+        return words;
+    }
+
     public static void main(String[] args) {
+        File file = new File("words.txt");
         Scanner scanner = new Scanner(System.in);
         int choice = 1;
         while (choice != 2) {
@@ -23,16 +39,22 @@ public class Game {
             switch (choice) {
                 case 1:
                     System.out.println("Start");
-                    System.out.println();
-                    for (String status: Status.STATUSES) {
-                        System.out.println(status);
-                        System.out.println();
-                    }
+                    System.out.println(readWords(file));
+//                    System.out.println();
+//                    for (String status: Status.STATUSES) {
+//                        System.out.println(status);
+//                        System.out.println();
+//                    }
                     break;
                 case 2:
                     System.out.println("Выход");
                     break;
             }
         }
+    }
+
+    private static void start() {
+        Random random = new Random();
+
     }
 }
