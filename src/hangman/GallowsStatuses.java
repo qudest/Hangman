6c6a02361afda85431c7bc8170d5e24b00c3d1d9
nagornyy_a_s <nagornyi.arsenii@mail.ru>
2024-02-03 16:56:42 +0300

    Refactoring

--- src/hangman/GallowsStatuses.java
+++ src/hangman/GallowsStatuses.java
@@ -1,54 +1,55 @@
 package hangman;
 
 public class GallowsStatuses {
-    public static final String ZERO = """
+    public static final String[] STATUSES = {
+            """
              ╒══╗
                 ║
                 ║
                 ║
              ═══╩═
-            """;
-    public static final String ONE = """
+            """,
+            """
              ╒══╗
              O  ║
                 ║
                 ║
              ═══╩═
-            """;
-    public static final String TWO = """
+            """,
+            """
              ╒══╗
              O  ║
              |  ║
                 ║
              ═══╩═
-            """;
-    public static final String THREE = """
+            """,
+            """
              ╒══╗
              O  ║
             /|  ║
                 ║
              ═══╩═
-            """;
-    public static final String FOUR = """
+            """,
+            """
              ╒══╗
              O  ║
             /|\\ ║
                 ║
              ═══╩═
-            """;
-    public static final String FIVE = """
+            """,
+            """
              ╒══╗
              O  ║
             /|\\ ║
             /   ║
              ═══╩═
-            """;
-    public static final String SIX = """
+            """,
+            """
              ╒══╗
              O  ║
             /|\\ ║
             / \\ ║
              ═══╩═
-            """;
-    public static final String[] STATUSES = {ZERO,ONE,TWO,THREE,FOUR,FIVE,SIX};
+            """
+    };
 }
